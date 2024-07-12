a76e7f02f78b6f328d87cb90bc8d10a7543fef63
Eoin Fennessy <efenness@redhat.com>
2024-07-12 15:32:13 +0100

    Initial pass at Service reconciliation

--- api/v1alpha1/zone_types.go
+++ api/v1alpha1/zone_types.go
@@ -17,26 +17,66 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // ZoneSpec defines the desired state of Zone
 type ZoneSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of Zone. Edit zone_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Namespaces is a list of namespaces to include in the Zone
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:MinItems=1
+	Namespaces []string `json:"namespaces"`
 }
 
 // ZoneStatus defines the observed state of Zone
 type ZoneStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Represents the latest available observations of the object's current state.
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
 }
+
+// SetStatusCondition sets the corresponding condition in conditions to newCondition and returns true
+// if the conditions are changed by this call.
+// conditions must be non-nil.
+//  1. if the condition of the specified type already exists (all fields of the existing condition are updated to
+//     newCondition, LastTransitionTime is set to now if the new status differs from the old status)
+//  2. if a condition of the specified type does not exist (LastTransitionTime is set to now() if unset, and newCondition is appended)
+func (s *ZoneStatus) SetStatusCondition(conditionType ZoneConditionType, status metav1.ConditionStatus, reason ZoneConditionReason, message string) bool {
+	return meta.SetStatusCondition(
+		&s.Conditions,
+		metav1.Condition{
+			Type:    string(conditionType),
+			Status:  status,
+			Reason:  string(reason),
+			Message: message,
+		},
+	)
+}
+
+// ZoneConditionType represents the type of the Zone's condition.
+// Zone condition types are: Reconciled
+type ZoneConditionType string
+
+// ZoneConditionReason indicates why the condition type is in its current state
+type ZoneConditionReason string
+
+const (
+	// ConditionTypeReconciled signifies whether the controller has successfully
+	// reconciled resources associated with a Zone.
+	ConditionTypeReconciled ZoneConditionType = "Reconciled"
+
+	// ConditionReasonReconcileInProgress indicates that the reconciliation
+	// of the Zone is progressing.
+	ConditionReasonReconcileInProgress ZoneConditionReason = "ReconcileInProgress"
+
+	// ConditionReasonReconcileError indicates that the reconciliation
+	// of the Zone has failed and will be retried.
+	ConditionReasonReconcileError ZoneConditionReason = "ReconcileError"
+
+	// ConditionReasonReconcileSuccess indicates that the reconciliation
+	// of the Zone has succeeded.
+	ConditionReasonReconcileSuccess ZoneConditionReason = "ReconcileSuccess"
+)
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
--- internal/controller/zone_controller.go
+++ internal/controller/zone_controller.go
@@ -18,13 +18,34 @@ package controller
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"strings"
 
+	"istio.io/api/annotation"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"reflect"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
-	multitenancyv1alpha1 "github.com/eoinfennessy/istio-multitenancy/api/v1alpha1"
+	"github.com/eoinfennessy/istio-multitenancy/api/v1alpha1"
+	"github.com/eoinfennessy/istio-multitenancy/pkg/constants"
+	pkgerrors "github.com/eoinfennessy/istio-multitenancy/pkg/errors"
+)
+
+const (
+	namespacesField string = ".spec.namespaces"
 )
 
 // ZoneReconciler reconciles a Zone object
@@ -36,27 +57,195 @@ type ZoneReconciler struct {
 // +kubebuilder:rbac:groups=multitenancy.istio.eoinfennessy.com,resources=zones,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=multitenancy.istio.eoinfennessy.com,resources=zones/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=multitenancy.istio.eoinfennessy.com,resources=zones/finalizers,verbs=update
+// +kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Zone object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.18.4/pkg/reconcile
-func (r *ZoneReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+func (r *ZoneReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, err error) {
+	log := logf.FromContext(ctx)
+	log.Info("Reconcile started")
 
-	// TODO(user): your logic here
+	// Fetch Zone resource
+	z := &v1alpha1.Zone{}
+	if err := r.Client.Get(ctx, req.NamespacedName, z); err != nil {
+		if apierrors.IsNotFound(err) {
+			log.Info("Zone resource not found; Ignoring because it must have been deleted")
+			return ctrl.Result{}, nil
+		} else {
+			log.Error(err, "Failed to fetch Zone resource; Requeuing")
+			return ctrl.Result{}, err
+		}
+	}
+
+	// Update status on return if different to old status
+	defer func(oldStatus *v1alpha1.ZoneStatus) {
+		if !reflect.DeepEqual(oldStatus, z.Status) {
+			if statusUpdateErr := r.Status().Update(ctx, z); statusUpdateErr != nil {
+				log.Error(err, "Failed to update Zone's status")
+				err = errors.Join(err, statusUpdateErr)
+			}
+		}
+	}(z.Status.DeepCopy())
+
+	// Add finalizer if not present
+	if !controllerutil.ContainsFinalizer(z, constants.ZoneFinalizer) {
+		if err := r.addFinalizer(ctx, z); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	// Handle deletion
+	if !z.GetDeletionTimestamp().IsZero() && controllerutil.ContainsFinalizer(z, constants.ZoneFinalizer) {
+		return ctrl.Result{}, r.finalize(ctx, z)
+	}
+
+	// Get lists of Services that should be part of the Zone
+	servicesLists := make([]*corev1.ServiceList, len(z.Spec.Namespaces))
+	ch := make(chan error)
+	for i, ns := range z.Spec.Namespaces {
+		servicesLists[i] = &corev1.ServiceList{}
+		go func(ch chan error) {
+			ch <- r.List(ctx, servicesLists[i], &client.ListOptions{Namespace: ns})
+		}(ch)
+	}
+	for range z.Spec.Namespaces {
+		err = <-ch
+		if err != nil {
+			log.Error(err, "Failed to list Services")
+			return ctrl.Result{}, err
+		}
+	}
+
+	// Set labels and annotations on Services
+	var serviceCount int
+	exportToAnnotationValue := strings.Join(z.Spec.Namespaces, ",")
+	for _, servicesList := range servicesLists {
+		serviceCount += len(servicesList.Items)
+		for _, service := range servicesList.Items {
+			go func(ch chan error) {
+				var metaChanged bool
+				if labelVal, exists := service.GetLabels()[constants.ZoneLabel]; exists {
+					// Return ZoneConflictError if Service is currently part of another Zone
+					if labelVal != req.Name {
+						err := &pkgerrors.ZoneConflictError{
+							Err: errors.New(fmt.Sprintf("Service %s in namespace %s is currently part of zone %s", service.Name, service.Namespace, labelVal)),
+						}
+						ch <- err
+						return
+					}
+				} else {
+					service.SetLabels(map[string]string{constants.ZoneLabel: req.Name})
+					metaChanged = true
+				}
+
+				if service.GetAnnotations()[annotation.NetworkingExportTo.Name] != exportToAnnotationValue {
+					service.SetAnnotations(map[string]string{annotation.NetworkingExportTo.Name: exportToAnnotationValue})
+					metaChanged = true
+				}
+				if metaChanged {
+					log.Info("Updating service", "namespace", service.Namespace, "name", service.Name)
+					ch <- r.Update(ctx, &service)
+				}
+				ch <- nil
+			}(ch)
+		}
+	}
+	for range serviceCount {
+		err = <-ch
+		if err != nil {
+			if zoneConflictErr, ok := err.(*pkgerrors.ZoneConflictError); ok {
+				z.Status.SetStatusCondition(v1alpha1.ConditionTypeReconciled, metav1.ConditionFalse, v1alpha1.ConditionReasonReconcileError, zoneConflictErr.Error())
+				return ctrl.Result{}, nil
+			} else {
+				z.Status.SetStatusCondition(v1alpha1.ConditionTypeReconciled, metav1.ConditionFalse, v1alpha1.ConditionReasonReconcileError, "Failed to update Service")
+				return ctrl.Result{}, err
+			}
+		}
+	}
 
 	return ctrl.Result{}, nil
 }
 
+func (r *ZoneReconciler) addFinalizer(ctx context.Context, z *v1alpha1.Zone) error {
+	log := logf.FromContext(ctx)
+	log.Info("Adding finalizer")
+	z.Status.SetStatusCondition(
+		v1alpha1.ConditionTypeReconciled,
+		metav1.ConditionFalse,
+		v1alpha1.ConditionReasonReconcileInProgress,
+		"Adding finalizer",
+	)
+	controllerutil.AddFinalizer(z, constants.ZoneFinalizer)
+	if err := r.Update(ctx, z); err != nil {
+		log.Error(err, "Failed to update Zone after adding finalizer")
+		z.Status.SetStatusCondition(
+			v1alpha1.ConditionTypeReconciled,
+			metav1.ConditionFalse,
+			v1alpha1.ConditionReasonReconcileError,
+			"Failed to update Zone",
+		)
+		return err
+	}
+	return nil
+}
+
+func (r *ZoneReconciler) finalize(ctx context.Context, z *v1alpha1.Zone) error {
+	log := logf.FromContext(ctx)
+	log.Info("Finalizing Zone resource")
+
+	// TODO: Add finalize logic
+
+	controllerutil.RemoveFinalizer(z, constants.ZoneFinalizer)
+	if err := r.Update(ctx, z); err != nil {
+		log.Error(err, "Failed to update Zone after removing finalizer")
+		return err
+	}
+	return nil
+}
+
+func (r *ZoneReconciler) mapServiceToReconcileRequests(ctx context.Context, service client.Object) []reconcile.Request {
+	zones := &v1alpha1.ZoneList{}
+	if err := r.List(ctx, zones, &client.ListOptions{
+		FieldSelector: fields.OneTermEqualSelector(
+			namespacesField, service.GetNamespace(),
+		),
+	}); err != nil {
+		return nil
+	}
+
+	requests := make([]reconcile.Request, len(zones.Items))
+	for i, z := range zones.Items {
+		requests[i] = reconcile.Request{
+			NamespacedName: types.NamespacedName{
+				Name: z.GetName(),
+			},
+		}
+	}
+	return requests
+}
+
 // SetupWithManager sets up the controller with the Manager.
 func (r *ZoneReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	// Create index for Zone on Namespaces field
+	if err := mgr.GetFieldIndexer().IndexField(
+		context.Background(),
+		&v1alpha1.Zone{},
+		namespacesField,
+		extractNamespacesIndex); err != nil {
+		return err
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&multitenancyv1alpha1.Zone{}).
+		For(&v1alpha1.Zone{}).
+		Watches(
+			&corev1.Service{},
+			handler.EnqueueRequestsFromMapFunc(r.mapServiceToReconcileRequests),
+			// TODO: Narrow down predicates: I think we only care if the labels or annotations have changed
+			builder.WithPredicates(predicate.ResourceVersionChangedPredicate{}),
+		).
 		Complete(r)
+}
+
+func extractNamespacesIndex(rawZone client.Object) []string {
+	return rawZone.(*v1alpha1.Zone).Spec.Namespaces
 }
